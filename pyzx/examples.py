c1c02b93b642393fc0c487350b07b8260c95395b
John van de Wetering <wetering@cs.ru.nl>
2018-06-06 04:46:22 +0200

    Added random Clifford circuit generator

--- pyzx/examples.py
+++ pyzx/examples.py
@@ -1,6 +1,8 @@
 __all__ = ['cnots','zigzag', 'zigzag2']
 
 import random
+from fractions import Fraction
+
 from .graph.graph import Graph
 
 
@@ -78,6 +80,105 @@ def cnots(qubits, depth, backend=None, keynames=('q','r')):
         g.set_vdata(v-i-1, 'o', True)
     return g
 
+def accept(p):
+    return p>random.random()
+
+def random_phase():
+    return Fraction(random.randint(1,4),2)
+
+def cliffords(qubits, depth, backend=None, keynames=('q','r')):
+    #randomness parameters
+    p_two_qubit = 0.4 #whether to add a edge between two qubits
+    p_cnot = 0.4 # whether to CNOT or to CZ
+    p_phase = 0.6 #probability of adding a phase to a node
+    p_had = 0.2 # probability of adding a hadamard on a qubit
+
+
+    g = Graph(backend)
+
+    # initialise and add input row
+
+    q = list(range(qubits))   # qubit index, initialised with input
+    r = 1                     # current rank
+    ty = [0] * qubits         # types of vertices
+    qs = list(range(qubits))  # tracks qubit indices of vertices
+    rs = [0] * qubits         # tracks rank of vertices
+    v = qubits                # next vertex to add
+    es1 = [] # normal edges to add
+    es2 = [] # hadamard edges to add
+    phases = {}
+
+    # initial row of Z
+    for i in range(qubits):
+        es1.append((q[i], v))
+        q[i] = v
+        rs.append(r)
+        qs.append(i)
+        ty.append(1)
+        v += 1
+    r += 1
+
+    # random gates
+    for i in range(depth):
+        c = random.randint(0, qubits-1)
+        t = random.randint(0, qubits-2)
+        if t >= c: t += 1
+        if accept(p_two_qubit):
+            if accept(p_cnot): es1.append((v, v+1))
+            else: es2.append((v,v+1))
+            if accept(p_phase): phases[v] = random_phase()
+            if accept(p_phase): phases[v+1] = random_phase()
+        else:
+            phases[v] = random_phase()
+            phases[v+1] = random_phase()
+        
+        if accept(p_had): es2.append((q[c],v))
+        else: es1.append((q[c],v))
+        if accept(p_had): es2.append((q[t],v+1))
+        else: es1.append((q[t],v+1))
+
+        q[c] = v
+        q[t] = v+1
+        
+        rs += [r,r]
+        qs += [c,t]
+        ty += [1,2]
+        v += 2
+        r += 1
+
+    # final row of Z
+    for i in range(qubits):
+        es1.append((q[i], v))
+        q[i] = v
+        rs.append(r)
+        qs.append(i)
+        ty.append(1)
+        v += 1
+    r += 1
+
+    # outputs
+    qs += list(range(qubits))
+    rs += [r] * qubits
+    ty += [0] * qubits
+    es1 += [(q[i], v+i) for i in range(qubits)]
+    v += qubits
+
+
+    g.add_vertices(v)
+    g.add_edges(es1,1)
+    g.add_edges(es2,2)
+
+    for i in range(v):
+        g.set_type(i, ty[i])
+        g.set_vdata(i, keynames[0], qs[i])
+        g.set_vdata(i, keynames[1], rs[i])
+    for v, phase in phases.items():
+        g.set_angle(v,phase)
+
+    for i in range(qubits):
+        g.set_vdata(i, 'i', True)
+        g.set_vdata(v-i-1, 'o', True)
+    return g
 
 def zigzag(sz, backend=None):
     g = Graph(backend)
