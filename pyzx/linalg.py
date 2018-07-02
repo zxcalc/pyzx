801fb5b2342c8d94452f8e420dcfdbc9c7d5bafd
John van de Wetering <wetering@cs.ru.nl>
2018-07-02 13:54:01 +0200

    Added copyright notice to every source file

--- pyzx/linalg.py
+++ pyzx/linalg.py
@@ -1,4 +1,21 @@
-class Mat2:
+# PyZX - Python library for quantum circuit rewriting 
+#        and optimisation using the ZX-calculus
+# Copyright (C) 2018 - Aleks Kissinger and John van de Wetering
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+class Mat2(object):
     """A matrix over Z2, with methods for multiplication, primitive row and column
     operations, Gaussian elimination, rank, and epi-mono factorisation."""
     
