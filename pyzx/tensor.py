34dd029b2da946e253bc505d1471c7e885bf34cb
John van de Wetering <wetering@cs.ru.nl>
2018-07-09 16:43:24 +0200

    Making tensors froms graphs is now reliable enough to compare ZX-graphs.

--- pyzx/tensor.py
+++ pyzx/tensor.py
@@ -24,72 +24,6 @@ except:
     np = None
 from math import pi
 
-qpi = 0.25*pi
-
-def contract_all(tensors,conns):
-    """
-    Contract the tensors inside the list tensors
-    according to the connectivities in conns
-
-    Due to limitations in numpy, only up to 26 contractions can be done.
-
-    Example input:
-    tensors = [np.random.rand(2,3),np.random.rand(3,4,5),np.random.rand(3,4)]
-    conns = [((0,1),(2,0)), ((1,1),(2,1))]
-    returned shape in this case is (2,3,5)
-    Taken from https://stackoverflow.com/questions/42034480/efficient-tensor-contraction-in-python
-    """
-
-    ndims = [t.ndim for t in tensors]
-    totdims = sum(ndims)
-    dims0 = np.arange(totdims)
-    # keep track of sublistout throughout
-    sublistout = set(dims0.tolist())
-    # cut up the index array according to tensors
-    # (throw away empty list at the end)
-    inds = np.split(dims0,np.cumsum(ndims))[:-1]
-    # we also need to convert to a list, otherwise einsum chokes
-    inds = [ind.tolist() for ind in inds]
-
-    # if there were no contractions, we'd call
-    # np.einsum(*zip(tensors,inds),sublistout)
-
-    # instead we need to loop over the connectivity graph
-    # and manipulate the indices
-    for (m,i),(n,j) in conns:
-        # tensors[m][i] contracted with tensors[n][j]
-
-        # remove the old indices from sublistout which is a set
-        sublistout -= {inds[m][i],inds[n][j]}
-
-        # contract the indices
-        inds[n][j] = inds[m][i]
-
-    #There are now 'holes' in the used indices.
-    #Since numpy only supports indices up to 26, we will
-    #lower the indices down as much as possible before continuing.
-    used_indices = set()
-    for l in inds:
-        used_indices.update(l)
-    remap = {k:v for (v,k) in enumerate(list(used_indices))}
-    for i in range(len(inds)):
-      inds[i] = [remap[j] for j in inds[i]]
-    s2 = set()
-    for i in sublistout:
-        s2.add(remap[i])
-    sublistout = list(s2)
-    sublistout.sort()
-    
-    print(inds)
-    print(sublistout)
-
-    # zip and flatten the tensors and indices
-    args = [subarg for arg in zip(tensors,inds) for subarg in arg]
-    args.append(sublistout)
-
-    # assuming there are no multiple contractions, we're done here
-    return np.einsum(*args)
-
 
 def Z_to_tensor(arity, phase):
     m = np.zeros([2]*arity, dtype = complex)
@@ -113,46 +47,79 @@ def X_to_tensor(arity, phase):
 #print(had)
 
 
+def pop_and_shift(verts, indices):
+    res = []
+    for v in verts:
+        res.append(indices[v].pop())
+    for i in sorted(res,reverse=True):
+        for w,l in indices.items():
+            l2 = []
+            for j in l:
+                if j>i: l2.append(j-1)
+                else: l2.append(j)
+            indices[w] = l2
+    return res
+
+
+# TODO: Support Hadamard gates
 def tensorfy(g):
     """Takes in a Graph and outputs a multidimensional numpy array
-    representing the linear map the ZX-diagram implements"""
-    tensors = []
-    int_vertices = {}
-
-    types = g.types()
+    representing the linear map the ZX-diagram implements.
+    Beware that quantum circuits take exponential memory to represent."""
+    rows = g.rows()
     phases = g.phases()
-
+    types = g.types()
+    depth = g.depth()
+    verts_row = {}
     for v in g.vertices():
-        if types[v] in (1,2):
+        r = rows[v]
+        if r in verts_row: verts_row[r].append(v)
+        else: verts_row[r] = [v]
+    
+    id2 = np.identity(2)
+    tensor = np.identity(2)
+    qubits = g.qubit_count()
+    for i in range(qubits-1): tensor = np.tensordot(tensor,id2,axes=0)
+    inputs = sorted(g.inputs,key=g.qubit)
+    indices = {}
+    for i in inputs:
+        indices[i] = [qubits + g.qubit(i)]
+    
+    for r in sorted(verts_row.keys()):
+        if r == 0 or r == depth: continue #inputs and outputs
+        for v in sorted(verts_row[r]):
+            neigh = list(g.neighbours(v))
             phase = pi*phases[v]
-            int_vertices[v] = len(tensors)
-            d = g.vertex_degree(v)
-            tensors.append(Z_to_tensor(d,phase) if types[v] == 1 else X_to_tensor(d,phase))
-        elif types[v] != 0:
-            raise Exception("Wrong type for node with index {!s}: {!s}".format(v,types[v]))
+            t = Z_to_tensor(len(neigh),phase) if types[v] == 1 else X_to_tensor(len(neigh),phase)
+            nn = list(filter(lambda n: rows[n]<r or (rows[n]==r and n<v), neigh))
+            #print(neigh, nn)
+            contr = pop_and_shift(nn,indices)
+            #print(contr)
+            tensor = np.tensordot(tensor,t,axes=(contr,list(range(len(contr)))))
+            indices[v] = list(range(len(tensor.shape)-len(neigh)+len(contr), len(tensor.shape)))
+            #print(indices)
+            #print(tensor)
+    
+    perm = inputs
+    for o in sorted(g.outputs,key=g.qubit):
+        n = g.neighbours(o)
+        if len(n) != 1: raise ValueError("Weird output")
+        n = list(n)[0]
+        if len(indices[n]) != 1: raise ValueError("Weird output")
+        inputs.append(indices[n][0])
+    
+    tensor = np.transpose(tensor,perm)
+        
+    return tensor
 
-    conns = []
-    contraction_count = {v:0 for v in int_vertices}
-    for v in g.inputs:
-        for n in g.neighbours(v): contraction_count[n] += 1
-    for e in g.edges():
-        s,t = g.edge_st(e)
-        if not (s in int_vertices and t in int_vertices): continue
-        conns.append(((int_vertices[s],contraction_count[s]),
-                     (int_vertices[t],contraction_count[t])))
-        contraction_count[s] += 1
-        contraction_count[t] += 1
 
-    #print(conns)
-
-    return contract_all(tensors, conns)
-
-
-
-if __name__ == '__main__':
-
-    import generate
-
-    g = generate.zigzag(7)
-
-    #print(zx_graph_to_tensor(g))
+def compare_tensors(t1,t2):
+    epsilon = 10**-10
+    if np.allclose(t1,t2): return True
+    for i,a in enumerate(t1.flat):
+        if abs(a)>epsilon: 
+            if abs(t2.flat[i])<epsilon: return False
+            break
+    else:
+        raise ValueError("Tensor is too close to zero")
+    return np.allclose(t1/a,t2/t2.flat[i])
