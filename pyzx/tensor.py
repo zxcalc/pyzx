c6f7331db2e20b9e75c1487da947a31f2834a206
John van de Wetering <wetering@cs.ru.nl>
2019-07-10 17:16:13 +0200

    typo in tensor code

--- pyzx/tensor.py
+++ pyzx/tensor.py
@@ -104,7 +104,7 @@ def tensorfy(g, preserve_scalar=True):
             tensor = np.tensordot(tensor,t,axes=(contr,list(range(len(t.shape)-len(contr),len(t.shape)))))
             indices[v] = list(range(len(tensor.shape)-d+len(contr), len(tensor.shape)))
             
-            if preserve_scalar and i % 10 == 0:
+            if not preserve_scalar and i % 10 == 0:
                 if np.abs(tensor).max() < 10**-6: # Values are becoming too small
                     tensor *= 10**4 # So scale all the numbers up
     perm = []
