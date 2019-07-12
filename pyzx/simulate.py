Fixed bug in path sum calculation

pyzx/simulate.py
@@ -72,7 +72,7 @@ class SumGraph(object):
 def calculate_path_sum(g):
     """Input should be a fully reduced scalar graph-like Clifford+T ZX-diagram. 
     Calculates the scalar it represents."""
-    if g.num_vertices() < 10: return g.to_tensor().flatten()[0]
+    if g.num_vertices() < 2: return g.to_tensor().flatten()[0]
     phases = g.phases()
     prefactor = 0
     variable_dict = dict()
@@ -94,11 +94,10 @@ def calculate_path_sum(g):
                     targets.add(variable_dict[t])
                 prefactor += len(targets)-1
                 xors[frozenset(targets)] = int(phases[v]*4)
-            continue
+                continue
         variable_dict[v] = len(variables)
         variables.append(int(phases[v]*4))
-    verts = list(variable_dict.keys())
-    variables = np.array(variables)
+    verts = sorted(list(variable_dict.keys()), key=lambda x: variable_dict[x])
     n = len(verts)
     for i in range(n):
         v = verts[i]
@@ -108,32 +107,33 @@ def calculate_path_sum(g):
                 czs.append((i,j))
                 prefactor += 1
 
-    # c = Circuit(n)
-    # for i in range(n):
-    #     c.add_gate("ZPhase",i,phase=Fraction(variables[i],4))
-    # for targets, phase in xors.items():
-    #     c.add_gate("ParityPhase", Fraction(phase,4), *list(targets))
-    # for i,j in czs:
-    #     c.add_gate("CZ", i,j)
-    # g2 = c.to_graph()
-    # g2.apply_state("+"*n)
-    # g2.apply_effect("+"*n)
-    # g2.scalar.add_power(2*n)
-    # val = g2.to_tensor().flatten()[0]
-    # return g.scalar.to_number()*val*sq2**(-prefactor)
-    xors = [(np.array([int(k in xor) for k in range(n)]),phase) for xor,phase in xors.items()]
-    print(n,len(czs),len(xors))
+    c = Circuit(n)
+    for i in range(n):
+        c.add_gate("ZPhase",i,phase=Fraction(variables[i],4))
+    for targets, phase in xors.items():
+        c.add_gate("ParityPhase", Fraction(phase,4), *list(targets))
+    for i,j in czs:
+        c.add_gate("CZ", i,j)
+    g2 = c.to_graph()
+    g2.apply_state("+"*n)
+    g2.apply_effect("+"*n)
+    g2.scalar.add_power(2*n)
+    val = g2.to_tensor().flatten()[0]
+    return g.scalar.to_number()*val*(sq2**(-prefactor))
+    # variables = np.array(variables)
+    # xors = [(np.array([int(k in xor) for k in range(n)]),phase) for xor,phase in xors.items()]
+    # print(n,len(czs),len(xors))
     
-    results = [0]*8
-    for bitstring in itertools.product([0,1],repeat=n):
-        val = np.dot(variables,bitstring)
-        val += sum(phase*(np.dot(bitstring,xor)%2) for xor,phase in xors)
-        val += 4*sum(1 for i,j in czs if bitstring[i] and bitstring[j])
-        try: results[val % 8] += 1
-        except: print(val)
-    print(results)
-    r = results
-    return g.scalar.to_number()*sq2**(-prefactor)*(r[0]-r[4]+omega*(r[1]-r[5]) +1j*(r[2]-r[6]) + 1j*omega*(r[3]-r[7]))
+    # results = [0]*8
+    # for bitstring in itertools.product([0,1],repeat=n):
+    #     val = np.dot(variables,bitstring)
+    #     val += sum(phase*(np.dot(bitstring,xor)%2) for xor,phase in xors)
+    #     val += 4*sum(1 for i,j in czs if bitstring[i] and bitstring[j])
+    #     try: results[val % 8] += 1
+    #     except: print(val)
+    # print(results)
+    # r = results
+    # return g.scalar.to_number()*sq2**(-prefactor)*(r[0]-r[4]+omega*(r[1]-r[5]) +1j*(r[2]-r[6]) + 1j*omega*(r[3]-r[7]))
 
 
 

tests/long_scalar_test.py
@@ -21,18 +21,23 @@ if __name__ == '__main__':
     sys.path.append('..')
     sys.path.append('.')
 
+import numpy as np
+
 from pyzx.tensor import compare_tensors
 from pyzx.generate import CNOT_HAD_PHASE_circuit
 from pyzx.simplify import clifford_simp, full_reduce
+from pyzx.simulate import calculate_path_sum
 from pyzx.circuit import Circuit
 
-SEED = 1337
+SEED = 1338
 random.seed(SEED)
 
 
 def compare(a,b):
-    if not compare_tensors(a, b):
-        raise AssertionError("Not equal")
+    try:
+        if not compare_tensors(a, b):
+            raise AssertionError("Not equal")
+    except (ValueError, MemoryError): pass
 
 
 def do_tests(qubits, depth, iterations):
@@ -42,7 +47,7 @@ def do_tests(qubits, depth, iterations):
             if i%25 == 0: print(i, end='.', flush=True)
             seed = random.randint(100000,500000)
             random.seed(seed)
-            c = CNOT_HAD_PHASE_circuit(qubits,depth,p_had=0.1, p_t=0.2)
+            c = CNOT_HAD_PHASE_circuit(qubits,depth,p_had=0.1, p_t=0.3)
             g = c.to_graph()
             g.apply_state(''.join(random.choice('+-01') for _ in range(qubits)))
             g.apply_effect(''.join(random.choice('+-01') for _ in range(qubits)))
@@ -50,12 +55,16 @@ def do_tests(qubits, depth, iterations):
             g2 = g.copy()
             clifford_simp(g2,quiet=True)
             steps = ["clifford_simp"]
-            compare(t, g2.to_tensor())
+            compare(t, g2)
 
             g2 = g.copy()
             full_reduce(g2,quiet=True)
             steps = ["full_reduce"]
-            compare(t, g2.to_tensor())
+            compare(t, g2)
+
+            val = calculate_path_sum(g2)
+            steps.append("calculate_path_sum")
+            compare(t,np.array([val]))
      
     except AssertionError:
         print("Unequality for circuit with seed {:d}, qubits {:d} and depth {:d}".format(seed, qubits, depth))
@@ -67,7 +76,8 @@ def do_tests(qubits, depth, iterations):
     else:
         print("\nTests finished successfully")
 
-do_tests(3, 20, 250)
-do_tests(3, 50, 250)
+do_tests(3, 20, 500)
+do_tests(3, 50, 500)
+do_tests(4,70,500)
 do_tests(5, 100, 250)
 do_tests(10, 100, 250)