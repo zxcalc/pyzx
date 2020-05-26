b94d8a37dbd0f5fa6e5aa8dc3659fa44f7cc41c2
John van de Wetering <john@vdwetering.name>
2020-05-26 17:12:21 +0200

    Changed License from GPL to Apache

--- pyzx/gflow.py
+++ pyzx/gflow.py
@@ -2,52 +2,53 @@
 #        and optimisation using the ZX-calculus
 # Copyright (C) 2018 - Aleks Kissinger and John van de Wetering
 
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+#    http://www.apache.org/licenses/LICENSE-2.0
 
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
+"""
+ Based on algorithm by Perdrix and Mhalla. Here is the pseudocode from
+dx.doi.org/10.1007/978-3-540-70575-8_70
 
-# Based on algorithm by Perdrix and Mhalla. Here is the pseudocode from
-# dx.doi.org/10.1007/978-3-540-70575-8_70
-#
-# input : An open graph
-# output: A generalised flow
-#
-# gFlow (V,Gamma,In,Out) =
-# begin
-#   for all v in Out do
-#     l(v) := 0
-#   end
-#   return gFlowaux (V,Gamma,In,Out,1)
-# end
-#
-# gFlowaux (V,Gamma,In,Out,k) =
-# begin
-#   C := {}
-#   for all u in V \ Out do
-#     Solve in F2 : Gamma[V \ Out, Out \ In] * I[X] = I[{u}]
-#     if there is a solution X0 then
-#       C := C union {u}
-#       g(u) := X0
-#       l(u) := k
-#     end
-#   end
-#   if C = {} then
-#     return (Out = V,(g,l))
-#   else
-#     return gFlowaux (V, Gamma, In, Out union C, k + 1)
-#   end
-# end
-#
+```
+input : An open graph
+output: A generalised flow
+
+gFlow (V,Gamma,In,Out) =
+begin
+  for all v in Out do
+    l(v) := 0
+  end
+  return gFlowaux (V,Gamma,In,Out,1)
+end
+
+gFlowaux (V,Gamma,In,Out,k) =
+begin
+  C := {}
+  for all u in V \ Out do
+    Solve in F2 : Gamma[V \ Out, Out \ In] * I[X] = I[{u}]
+    if there is a solution X0 then
+      C := C union {u}
+      g(u) := X0
+      l(u) := k
+    end
+  end
+  if C = {} then
+    return (Out = V,(g,l))
+  else
+    return gFlowaux (V, Gamma, In, Out union C, k + 1)
+  end
+end
+```
+"""
 
 from typing import Dict, Set, Tuple, Optional
 
--- pyzx/graph/graph_gt.py
+++ pyzx/graph/graph_gt.py
@@ -1,3 +1,19 @@
+# PyZX - Python library for quantum circuit rewriting 
+#        and optimisation using the ZX-calculus
+# Copyright (C) 2018 - Aleks Kissinger and John van de Wetering
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#    http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import graph_tool.all as gt
 
 from .base import BaseGraph
