6bedc8ece02f71dc1f237868b9e68d41cb0af988
John van de Wetering <wetering@cs.ru.nl>
2020-08-01 13:36:50 +0200

    Fixed bug where Graph.num_edges() wouldn't return correct number of edges in graph

--- pyzx/graph/graph_s.py
+++ pyzx/graph/graph_s.py
@@ -1,12 +1,12 @@
 # PyZX - Python library for quantum circuit rewriting 
-#        and optimization using the ZX-calculus
+#		and optimization using the ZX-calculus
 # Copyright (C) 2018 - Aleks Kissinger and John van de Wetering
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 
-#    http://www.apache.org/licenses/LICENSE-2.0
+#	http://www.apache.org/licenses/LICENSE-2.0
 
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -112,7 +112,8 @@ class GraphS(BaseGraph[int,Tuple[int,int]]):
 		return len(self.graph)
 
 	def num_edges(self):
-		return self.nedges
+		#return self.nedges
+		return sum(len(adj) for adj in self.graph.values())
 
 	def vertices(self):
 		return self.graph.keys()
