d7eec38fb1aeb83699bb4ffb0d61ffbc3155a8b7
mjsutcliffe99 <105458229+mjsutcliffe99@users.noreply.github.com>
2024-04-23 17:41:53 +0100

    Added tests for cutting
    Added tests for vertex and edge cutting on random graphs to verify the scalar results are correct

--- tests/test_simulate.py
+++ tests/test_simulate.py
@@ -17,6 +17,7 @@
 
 import unittest
 import sys
+import random
 from types import ModuleType
 from typing import Optional
 
@@ -24,7 +25,9 @@ if __name__ == '__main__':
     sys.path.append('..')
     sys.path.append('.')
 from pyzx.circuit import Circuit
-from pyzx.simulate import replace_magic_states
+from pyzx.simulate import replace_magic_states, cut_vertex, cut_edge
+from pyzx.generate import cliffords
+from pyzx.simplify import full_reduce
 
 np: Optional[ModuleType]
 try:
@@ -32,6 +35,14 @@ try:
 except ImportError:
     np = None
 
+def rand_graph(qubits=5,depth=10):
+    g = cliffords(qubits,depth)
+    g.apply_state('0'*qubits)
+    g.apply_effect('0'*qubits)
+    return g
+    
+def round_complex(scalar,decimal_places):
+    return round(scalar.real,decimal_places) + round(scalar.imag,decimal_places)*1j
 
 @unittest.skipUnless(np, "numpy needs to be installed for this to run")
 class TestSimulate(unittest.TestCase):
@@ -43,6 +54,32 @@ class TestSimulate(unittest.TestCase):
         g = c.to_graph()
         gsum = replace_magic_states(g)
         self.assertTrue(np.allclose(g.to_tensor(), gsum.to_tensor()))
+        
+    def test_vertex_cut(self,repeats=20):
+        for i in range(1,repeats):
+            g = rand_graph() # generate random Clifford graph
+            v_cut = random.randrange(len(g.vertices()))
+            g0,g1 = cut_vertex(g,v_cut) # apply random vertex cut
+            
+            for g_i in (g,g0,g1): full_reduce(g_i)
+            
+            scal    = round_complex(g.scalar.to_number(),3) # the scalar from fully reducing g
+            scalCut = round_complex(g0.scalar.to_number()+g1.scalar.to_number(),3) # the sum of scalars from the cut graph
+            assert(scal == scalCut)
+        
+    def test_edge_cut(self,repeats=20):
+        for i in range(1,repeats):
+            g = rand_graph() # generate random Clifford graph
+            rand_v = random.randrange(len(g.vertices()))
+            rand_neigh = list(g.neighbors(rand_v))[random.randrange(len(g.neighbors(rand_v)))]
+            e_cut = (rand_v,rand_neigh)  # apply random edge cut
+            
+            g0,g1 = cut_edge(g,e_cut)
+            for g_i in (g,g0,g1): full_reduce(g_i)
+            
+            scal    = round_complex(g.scalar.to_number(),3) # the scalar from fully reducing g
+            scalCut = round_complex(g0.scalar.to_number()+g1.scalar.to_number(),3) # the sum of scalars from the cut graph
+            assert(scal == scalCut)
 
 
 if __name__ == '__main__':
