8fecde1d78ec1a6024658f15dffc4805009e71d6
mehatamm <mati@ehatamm.com>
2024-11-11 20:10:20 -0500

    force algorithm wip

--- pyzx/drawing.py
+++ pyzx/drawing.py
@@ -56,7 +56,6 @@ def draw(g: Union[BaseGraph[VT,ET], Circuit], labels: bool=False, **kwargs) -> A
     # allow global setting to labels=False
     # TODO: probably better to make labels Optional[bool]
     labels = labels or settings.show_labels
-    
     if get_mode() == "shell":
         return draw_matplotlib(g, labels, **kwargs)
     elif get_mode() == "browser":
@@ -288,13 +287,54 @@ random_graphid = random.Random()
 #     library_code += '</script>'
 #     display(HTML(library_code))
 
+def auto_draw_vertex_locs(g:BaseGraph[VT, ET], x_scale = 1, y_scale = 1): #Force-based graph drawing algorithm given by Eades(1984):
+    c1 = 2
+    c2 = 1
+    c3 = 1
+    c4 = .1
+    v_loc:Dict[VT, Tuple[int, int]] = dict()
+    for v in g.vertices():
+        v_loc[v]=(random.random()*10, 0 if v in g.inputs() else (1 if v in g.outputs() else random.random()*10))
+    for i in range(10): #100 iterations of force-based drawing
+        print(v_loc.values())
+        forces:Dict[VT, Tuple[int, int]] = dict()
+        for v in g.vertices():
+            forces[v] = (0, 0)
+            for v1 in g.vertices():
+                diff = (v_loc[v][0]-v_loc[v1][0], v_loc[v][1]-v_loc[v1][1])
+                d = math.sqrt(diff[0]*diff[0]+diff[1]*diff[1])
+                if g.connected(v1, v): #edge between vertices: apply rule c1*log(d/c2)
+                    force_mag = -c1*math.log(d/c2) #negative force attracts
+                elif v != v1: #nonadjacent vertices: apply rule -c3/d^2
+                    force_mag = c3/(d*d) #positive force repels
+                else: #free body in question, applies no force on itself
+                    force_mag = 0
+                v_force = (diff[0]*force_mag*c4, diff[1]*force_mag*c4)
+                forces[v] = (forces[v][0]+v_force[0], forces[v][1]+v_force[1])
+        for v in g.vertices(): #leave y value constant if input or output
+            v_loc[v]=(v_loc[v][0]+forces[v][0], 0 if v in g.inputs() else (1 if v in g.outputs() else v_loc[v][1]+forces[v][1]))
+    max_x = max(v[0] for v in v_loc.values())
+    min_x = min(v[0] for v in v_loc.values())
+    max_y = max(v[1] for v in v_loc.values())
+    min_y = min(v[1] for v in v_loc.values())
+    #scale_x = x_scale / (max_x - min_x)
+    #scale_y = y_scale / (max_y - min_y)
+    scale_x = x_scale
+    scale_y = y_scale
+    
+    v_loc = {k:((v[0]+min_x)*scale_x, (v[1]+min_y)*scale_y) for k, v in v_loc.items()} #rescale
+    return v_loc
+
+
+
 def draw_d3(
     g: Union[BaseGraph[VT,ET], Circuit],
     labels:bool=False, 
     scale:Optional[FloatInt]=None, 
     auto_hbox:Optional[bool]=None,
     show_scalar:bool=False,
-    vdata: List[str]=[]
+    vdata: List[str]=[],
+    auto_draw = False
     ) -> Any:
 
     if get_mode() not in ("notebook", "browser"): 
@@ -325,10 +365,12 @@ def draw_d3(
 
     w = (maxrow-minrow + 2) * scale
     h = (maxqub-minqub + 3) * scale
+    if(auto_draw):
+        v_dict = auto_draw_vertex_locs(g, w, h)
 
     nodes = [{'name': str(v),
-              'x': (g.row(v)-minrow + 1) * scale,
-              'y': (g.qubit(v)-minqub + 2) * scale,
+              'x': v_dict[v][0] if auto_draw else (g.row(v)-minrow + 1) * scale,
+              'y': v_dict[v][1] if auto_draw else (g.qubit(v)-minqub + 2) * scale,
               't': g.type(v),
               'phase': phase_to_s(g.phase(v), g.type(v)) if g.type(v) != VertexType.Z_BOX else str(get_z_box_label(g, v)),
               'ground': g.is_ground(v),
--- tests/test_drawing.py
+++ tests/test_drawing.py
@@ -1,11 +1,59 @@
 import sys, os; sys.path.append('..')
+import importlib
 from pyzx.graph.graph import Graph
+from pyzx.graph.base import BaseGraph, VT, ET
+import random
+from typing import *
+import  math
 from pyzx.drawing import draw
 from pyzx.utils import VertexType, EdgeType
+
+
+def auto_draw_vertex_locs(g:BaseGraph[VT, ET], x_scale = 1, y_scale = 1): #Force-based graph drawing algorithm given by Eades(1984):
+    c1 = 2
+    c2 = 1
+    c3 = 1
+    c4 = .1
+    v_loc:Dict[VT, Tuple[int, int]] = dict()
+    for v in g.vertices():
+        v_loc[v]=(random.random()*10,  random.random()*10)
+    for i in range(10): #100 iterations of force-based drawing
+        print(v_loc.values())
+        forces:Dict[VT, Tuple[int, int]] = dict()
+        for v in g.vertices():
+            forces[v] = (0, 0)
+            for v1 in g.vertices():
+                diff = (v_loc[v][0]-v_loc[v1][0], v_loc[v][1]-v_loc[v1][1])
+                d = math.sqrt(diff[0]*diff[0]+diff[1]*diff[1])
+                if g.connected(v1, v): #edge between vertices: apply rule c1*log(d/c2)
+                    force_mag = -c1*math.log(d/c2) #negative force attracts
+                elif v != v1: #nonadjacent vertices: apply rule -c3/d^2
+                    force_mag = c3/(d*d) #positive force repels
+                else: #free body in question, applies no force on itself
+                    force_mag = 0
+                v_force = (diff[0]*force_mag*c4, diff[1]*force_mag*c4)
+                forces[v] = (forces[v][0]+v_force[0], forces[v][1]+v_force[1])
+        for v in g.vertices(): #leave y value constant if input or output
+            v_loc[v]=(v_loc[v][0]+forces[v][0], v_loc[v][1]+forces[v][1])
+    max_x = max(v[0] for v in v_loc.values())
+    min_x = min(v[0] for v in v_loc.values())
+    max_y = max(v[1] for v in v_loc.values())
+    min_y = min(v[1] for v in v_loc.values())
+    #scale_x = x_scale / (max_x - min_x)
+    #scale_y = y_scale / (max_y - min_y)
+    #scale_x = x_scale
+    #scale_y = y_scale
+    
+    #v_loc = {k:((v[0]+min_x)*scale_x, (v[1]+min_y)*scale_y) for k, v in v_loc.items()} #rescale
+    return v_loc
 
 
 g = Graph()
 v1 = g.add_vertex(VertexType.BOUNDARY)
 v2 = g.add_vertex(VertexType.BOUNDARY)
+v3 = g.add_vertex(VertexType.BOUNDARY)
 g.add_edge(g.edge(v1, v2))
-draw(g)
+g.add_edge(g.edge(v2, v3))
+g.add_edge(g.edge(v1, v3))
+#draw(g)
+auto_draw_vertex_locs(g, .1, .1)
