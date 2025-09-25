db3dc9997f6e966111b4b888362536e7b003a9b7
Razin Shaikh <razinshaikh@outlook.com>
2025-09-25 22:52:13 +0100

    remove variable_types dictionary

--- pyzx/graph/base.py
+++ pyzx/graph/base.py
@@ -99,7 +99,6 @@ class BaseGraph(Generic[VT, ET], metaclass=DocstringMeta):
         # merge_vdata(v0,v1) is an optional, custom function for merging
         # vdata of v1 into v0 during spider fusion etc.
         self.merge_vdata: Optional[Callable[[VT,VT], None]] = None
-        self.variable_types: Dict[str,bool] = dict() # mapping of variable names to their type (bool or continuous) - legacy
         self.var_registry: VarRegistry = VarRegistry() # registry for variable types
 
     # MANDATORY OVERRIDES {{{
