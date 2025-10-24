41882f2fd1c666c26ad21ff5a82541b73dd2ecd6
lara <laraboloni@gmail.com>
2025-10-24 06:49:56 +0200

    can make order of vertex matching important

--- pyzx/rewrite.py
+++ pyzx/rewrite.py
@@ -170,12 +170,15 @@ class RewriteDoubleVertex(Rewrite[VT, ET]):
 
 class RewriteSimpDoubleVertex(RewriteDoubleVertex[VT, ET]):
     simp_match: Optional[Callable[[BaseGraph[VT, ET], VT, VT], bool]]
+    is_ordered: bool
 
     def __init__(self, is_match: Callable[[BaseGraph[VT, ET], VT, VT], bool],
                  applier: Callable[[BaseGraph[VT, ET], VT, VT], bool],
-                 simp_match: Optional[Callable[[BaseGraph[VT, ET], VT, VT], bool]] = None) -> None:
+                 simp_match: Optional[Callable[[BaseGraph[VT, ET], VT, VT], bool]] = None,
+                 is_ordered: bool = False) -> None:
         super().__init__(is_match, applier)
         self.simp_match = simp_match
+        self.is_ordered = is_ordered
 
     def find_all_matches (self, graph: BaseGraph[VT, ET]) -> Set[Tuple[VT, VT]]:
         all_matches: Set[Tuple[VT, VT]] = set()
@@ -187,7 +190,7 @@ class RewriteSimpDoubleVertex(RewriteDoubleVertex[VT, ET]):
         for v1 in graph.vertices():
             for v2 in graph.neighbors(v1):
                 if match(graph, v1, v2):
-                    pair = (v1, v2) if v1 <= v2 else (v2, v1)
+                    pair = (v1, v2) if (self.is_ordered or v1 <= v2) else (v2, v1)
                     all_matches.add(pair)
         return all_matches
 
@@ -232,14 +235,17 @@ class RewriteTripleVertex(Rewrite[VT, ET]):
 
 class RewriteSimpTripleVertex(RewriteTripleVertex[VT, ET]):
     simp_match: Optional[Callable[[BaseGraph[VT, ET], VT, VT, VT], bool]]
+    is_ordered: bool
 
     def __init__(self, is_match: Callable[[BaseGraph[VT, ET], VT, VT, VT], bool],
                  applier: Callable[[BaseGraph[VT, ET], VT, VT, VT], bool],
-                 simp_match: Optional[Callable[[BaseGraph[VT, ET], VT, VT, VT], bool]] = None) -> None:
+                 simp_match: Optional[Callable[[BaseGraph[VT, ET], VT, VT, VT], bool]] = None,
+                 is_ordered: bool = False) -> None:
         super().__init__(is_match, applier)
         self.simp_match = simp_match
+        self.is_ordered = is_ordered
 
-    def find_all_matches (self, graph: BaseGraph[VT, ET]) -> Set[Tuple[VT, ...]]:
+    def find_all_matches (self, graph: BaseGraph[VT, ET]) -> Set[Tuple[VT,...]]:
         all_matches: Set[Tuple[VT,...]] = set()
         if self.simp_match is not None:
             match = self.simp_match
@@ -250,8 +256,13 @@ class RewriteSimpTripleVertex(RewriteTripleVertex[VT, ET]):
             for v2 in graph.neighbors(v1):
                 for v3 in graph.neighbors(v2):
                     if match(graph, v1, v2, v3):
-                        pair = (v1, v2, v3)
-                        all_matches.add(pair)
+                        if self.is_ordered:
+                            all_matches.add((v1, v2, v3))
+                        else:
+                            m = list((v1, v2, v3))
+                            m.sort()
+                            m = tuple(m)
+                            all_matches.add(m)
         return all_matches
 
     def simp(self, graph: BaseGraph[VT, ET]) -> bool:
